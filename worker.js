4a2a2388e68c532d8634fac0d07c8e5edc086e11
NachoNekoNya <1210472556@qq.com>
2023-02-04 18:38:00 +0800

    简化代码

--- worker.js
+++ worker.js
@@ -8,19 +8,28 @@ var imgHost = "https://raw.githubusercontent.com/Cheshire-Nya/easy-random-img-ap
 var min = 1;
 var max;
 
+var maxValues = {
+  '/%E7%A4%BA%E4%BE%8B%E5%9B%BE': 10, //示例图
+  //判断当前访问的url.pathname是否为/示例图（即/%E7%A4%BA%E4%BE%8B%E5%9B%BE，js内中文不编码无法正常使用）
+  //是则max=10（即文件夹"示例图"下图片命名数字最大一个）
+  //其他路径下同理，只需要这样相同格式多写一条键值对即可`'/<文件夹名>': <数值>,`
+  '/demoimg': 5, //demoimg
+  //英文路径正常使用
+}
+//存储仓库下图片文件夹名称及对应的图片数
+
 async function handleRequest(request) {
   let nowUrl = new URL(request.url);
   let imgPath = nowUrl.pathname;
   
-  if (imgPath == '/%E7%A4%BA%E4%BE%8B%E5%9B%BE') { max=10;} //示例图
-  //判断当前访问的url.pathname是否为/示例图（即/%E7%A4%BA%E4%BE%8B%E5%9B%BE，js内中文不编码无法正常使用）
-  //是则max=10（即文件夹"示例图"下图片命名数字最大一个）
-  //其他路径下同理，只需要这样多写一次if即可
-  if (imgPath == '/demoimg') { max=5;} //demoimg
-  //英文路径正常使用
+  let max = maxValues[imgPath];
 
   let imgUrl = imgHost + imgPath + "/" + Math.floor(Math.random()*(max-min+1)+min) + ".jpg";
-  //图片应统一为jpg
+  
   let getimg = new Request(imgUrl);
-  return fetch(getimg);  
+  return fetch(getimg, {
+    headers: {
+      'content-type': 'image/jpeg'
+    },
+  });  
 }
